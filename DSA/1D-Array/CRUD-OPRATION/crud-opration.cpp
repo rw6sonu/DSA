26e807ede40171522e253334e3037a2d5177d0c8
sakshi <sakshisjpatel@gmail.com>
2025-07-17 14:55:03 +0530

    update

---
+++ DSA/1D-Array/CRUD-OPRATION/crud-opration.cpp
@@ -0,0 +1,90 @@
+// CRUD OPration
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, element, size, position, choice, count = 1;
+
+    cout << "enter the size of array:- ";
+    cin >> size;
+
+    int arr[size];
+    do
+    {
+
+        cout << " 1 element inserted in array" << endl;
+        cout << " 2 element deleted from array" << endl;
+        cout << " 3 element updated in array" << endl;
+        cout << " 4 element view in array" << endl;
+        cout << " 5 size of array" << endl;
+        cout << " 0 exit" << endl;
+
+        cout << "enter your Choice:- ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+
+            if (count >= size)
+            {
+                cout << " Array is Full";
+            }
+            else
+            {
+                cout << "enter the element:- ";
+                cin >> element;
+
+                cout << "enter the Position";
+                cin >> position;
+
+                if (position < 0 || position > count)
+                {
+                    cout << "Invalid Position";
+                }
+
+                else
+                {
+                    for (i = position; i < count; i++)
+                    {
+                        arr[i + 1] = arr[i];
+                    }
+                    arr[position] = element;
+                    count++;
+                    cout << "Element Inserted Successfully" << endl;
+                }
+            }
+            break;
+
+        case 2:
+
+            cout << "enter the element:- ";
+            cin >> element;
+
+            cout << "enter the Position";
+            cin >> position;
+
+            break;
+
+        case 3:
+            cout << "enter the element:- ";
+            cin >> element;
+            cout << "enter the Position";
+            cin >> position;
+
+            break;
+
+        case 4:
+            cout<<"your array is:- ";
+            for (i = 0; i < count; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+            break;
+        }
+       
+
+    } while (choice != 0);
+}
